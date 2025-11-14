0bd157cb21d3c0df9ff1fdb37a1785f4def9989a
DamianPrograme <dami.ramos@duocuc.cl>
2025-11-14 11:27:25 -0300

    Creacion del Proyecto 4 vistas Ingreso con admin vista menu tienda y producto

---
+++ app/src/main/java/com/example/proyecto/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.proyecto
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edUsername: EditText = findViewById(R.id.edUsername)
+        val edPasswd: EditText = findViewById(R.id.edPasswd)
+        val btnIngreso: Button = findViewById(R.id.btnIngresar)
+        val txMensaje: TextView = findViewById(R.id.txMensaje)
+
+        var defUsername = "admin"
+        var defPasswd = "admin"
+
+        btnIngreso.setOnClickListener {
+            if(edUsername.text.toString() == defUsername.toString()
+                && edPasswd.text.toString() == defPasswd.toString()){
+                val nuevaVentana = Intent(this, MainActivity2::class.java)
+                nuevaVentana.putExtra("par_usern",edUsername.text.toString())
+                startActivity(nuevaVentana)
+            }else{
+                txMensaje.text = "Error Usuario/Contraseña"
+            }
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/MainActivity2.kt
@@ -0,0 +1,58 @@
+package com.example.proyecto
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val LvOpciones: ListView = findViewById(R.id.LvOpciones)
+        val txUsuario: TextView = findViewById(R.id.txUsuario)
+        val recibirUsername = intent.getStringExtra("par_usern")
+
+        txUsuario.text = recibirUsername.toString()
+
+        val opcionesArr = arrayOf(
+            "Tienda",
+            "Producto"
+        )
+
+        val adaptador = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, opcionesArr
+        )
+
+        LvOpciones.adapter = adaptador
+
+        LvOpciones.setOnItemClickListener { parent, view, position, id ->
+            val itemElegido = parent.getItemAtPosition(position).toString()
+
+            if (itemElegido == "Tienda") {
+                val abrirTienda = Intent(this, MainActivity3::class.java)
+                startActivity(abrirTienda)
+            } else if (itemElegido == "Producto") {
+                val abrirProducto = Intent(this, MainActivity4::class.java)
+                startActivity(abrirProducto)
+
+            }
+
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
